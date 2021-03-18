3e83a09b46180c1f2123d3453c1a44ad202ec1a9
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-18 11:36:11 +0100

    Commit #4
    Creazione metodi lato client #3

--- client-kezapp/src/app/app.component.ts
+++ client-kezapp/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Chat } from './chat';
 import { InviaMessaggioDto } from './invia-messaggio-dto';
 import { Messaggio } from './messaggio';
 import { RegistrazioneDto } from './registrazioneDto';
+import { RichiediRegistrazioneDto } from './richiedi-registrazione-dto';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +14,35 @@ import { RegistrazioneDto } from './registrazioneDto';
 })
 export class AppComponent {
 
-  messaggio: Messaggio = new Messaggio();
+  messaggio: string = ""
+  nickname: string = "";
   listaMessaggi: Messaggio[] = [];
+  contatti: Chat[] = [];
+  chat: Chat;
 
   constructor(private http: HttpClient) {
   }
   ngOnInit() { }
 
-  conferma() {
+  
 
-    let dto: InviaMessaggioDto = new InviaMessaggioDto();
-    dto.messaggio = this.messaggio;
+    registrazione() {
 
-    let oss: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
-      "http://localhost:8080/conferma", dto);
+      let dto: RichiediRegistrazioneDto = new RichiediRegistrazioneDto();
+      dto.nickname = this.nickname;
+  
+      let oss: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
+        "http://localhost:8080//registrazione", dto);
+  
+      oss.subscribe
+        (c => {
+          this.contatti = c.contatti;
+          this.listaMessaggi = c.listaMessaggi;
+          this.chat.sessione = c.sessione
+        });
 
-    oss.subscribe(o => this.listaMessaggi = o.listaMessaggi);
+  }
+  inviaTutti(){
 
   }
 
